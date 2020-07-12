125fef7287e2810b259cf1f8f36b69bc039f49eb
equipe-guardian <noreply@bbseguros.equipe-guardian.com.br>
2020-07-12 19:06:00 -0300

    tradução pt-br adicionada

--- resources/lang/pt_BR/passwords.php
+++ resources/lang/pt_BR/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Sua senha foi resetada.',
+    'sent' => 'Enviamos um e-mail com instruções, por favor verifique-o.',
+    'throttled' => 'Espere até poder tentar novamente.',
+    'token' => 'Token inválido.',
+    'user' => "Esse email é inválido.",
 
 ];
